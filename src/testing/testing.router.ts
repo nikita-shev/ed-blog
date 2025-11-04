5d63f3a1d82a7d083222e1c0e56701c1dbb37a3c
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2025-11-04 15:53:54 +0300

    Update testing router (use mongodb).

--- src/testing/testing.router.ts
+++ src/testing/testing.router.ts
@@ -1,12 +1,12 @@
 import { Request, Response, Router } from 'express';
 import { HttpStatus } from '../core/constants/http-statuses';
-import { db } from '../db/db';
+import { blogCollection, postCollection } from '../db/db.config';
 
 export const testingRouter = Router();
 
-testingRouter.delete('/all-data', (req: Request, res: Response) => {
-    db.blogs = [];
-    db.posts = [];
+testingRouter.delete('/all-data', async (req: Request, res: Response) => {
+    await blogCollection.deleteMany();
+    await postCollection.deleteMany();
 
     res.sendStatus(HttpStatus.NoContent);
 });

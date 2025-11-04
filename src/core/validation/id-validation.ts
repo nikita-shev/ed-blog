21f962e42746beaaf0a610c3b2be579f4d493c09
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2025-11-04 16:34:26 +0300

    Update testing router (v2).

--- src/core/validation/id-validation.ts
+++ src/core/validation/id-validation.ts
@@ -4,4 +4,4 @@ export const idValidation = param('id')
     .exists()
     .withMessage('id is required')
     .isMongoId()
-    .withMessage("id is incorrect'");
+    .withMessage('id is incorrect');
--- src/testing/testing.router.ts
+++ src/testing/testing.router.ts
@@ -5,8 +5,7 @@ import { blogCollection, postCollection } from '../db/db.config';
 export const testingRouter = Router();
 
 testingRouter.delete('/all-data', async (req: Request, res: Response) => {
-    await blogCollection.deleteMany();
-    await postCollection.deleteMany();
+    await Promise.all([blogCollection.deleteMany(), postCollection.deleteMany()]);
 
     res.sendStatus(HttpStatus.NoContent);
 });

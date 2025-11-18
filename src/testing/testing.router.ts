c57640dd2e25ba00911d04e69c0b670ff71479ff
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2025-11-18 19:28:48 +0300

    Fix testingRouter.

--- src/testing/testing.router.ts
+++ src/testing/testing.router.ts
@@ -1,11 +1,15 @@
 import { Request, Response, Router } from 'express';
 import { HttpStatus } from '../core/constants/http-statuses';
-import { blogCollection, postCollection } from '../db/db.config';
+import { blogCollection, postCollection, userCollection } from '../db/db.config';
 
 export const testingRouter = Router();
 
 testingRouter.delete('/all-data', async (req: Request, res: Response) => {
-    await Promise.all([blogCollection.deleteMany(), postCollection.deleteMany()]);
+    await Promise.all([
+        blogCollection.deleteMany(),
+        postCollection.deleteMany(),
+        userCollection.deleteMany()
+    ]);
 
     res.sendStatus(HttpStatus.NoContent);
 });

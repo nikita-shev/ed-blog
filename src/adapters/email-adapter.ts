8a32ec3f68a3f2844f0addd47df01298f6cb5d02
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2026-03-16 14:34:53 +0300

    Fix email message(1).

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -121,7 +121,7 @@ export const messagesForEmail = {
         return `
                     <h1>Password recovery</h1>
                     <p>To finish password recovery please follow the link below:
-                        <a href='https://somesite.com/password-recovery?recoveryCode=${code}'>recovery password</a>
+                        <a href='https://somesite.com/new-password?code=${code}'>recovery password</a>
                     </p>
                 `;
     }
